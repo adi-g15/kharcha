a68e1819bccad992d14d4a615a9804cc58c54f58
Aditya Gupta <ag15035@gmail.com>
2023-03-05 18:27:37 +0530

    feat: add 'will_be_back' options

--- node.js
+++ node.js
@@ -13,15 +13,26 @@ if(!!process.argv[3]) {
 var data = require((process.argv[2][0] == "/" ? "":"./") + process.argv[2]);
 
 let total = {};
+let will_be_back = {};
 
 function detailedSummary() {
-    for (let e of data) if(!e.ignore) total[e.type] = 0;
-    for (let e of data) if(!e.ignore) total[e.type] += e.debit - e.credit;
+    for (let e of data) if(!e.ignore) { total[e.type] = 0; will_be_back[e.type] = 0; }
+    for (let e of data) if(!e.ignore) { total[e.type] += e.debit - e.credit; will_be_back[e.type] += (e.will_be_back || 0); }
 }
 
 function briefSummary() {
-    for (let e of data) if(!e.ignore) total[e.type.substr(0, (e.type.indexOf("/")!=-1)?e.type.indexOf("/"):e.type.length)] = 0;
-    for (let e of data) if(!e.ignore) total[e.type.substr(0, (e.type.indexOf("/")!=-1)?e.type.indexOf("/"):e.type.length)] += e.debit - e.credit;
+    for (let e of data) if(!e.ignore) {
+	    const idx = e.type.indexOf("/")
+	    const type = e.type.substr(0, (idx != -1) ? idx: e.type.length)
+	    total[type] = 0;
+	    will_be_back[type] = 0
+    }
+    for (let e of data) if(!e.ignore) {
+	    const idx = e.type.indexOf("/")
+	    const type = e.type.substr(0, (idx != -1) ? idx: e.type.length)
+	    total[type] += e.debit - e.credit;
+	    will_be_back[type] += (e.will_be_back || 0);
+    }
 }
 
 if (detailed) detailedSummary();
@@ -29,7 +40,12 @@ else briefSummary()
 
 let sum = 0;
 for (let key of Object.keys(total).sort()) {
-    console.log(key + ": " + total[key]);
-    sum += total[key];
+    if (will_be_back[key] > 0) {
+	console.log(key + ": " + total[key] + "\t( -" + will_be_back[key] + " )");
+    } else {
+	console.log(key + ": " + total[key]);
+    }
+
+    sum += total[key] - will_be_back[key];
     console.log("\t", sum);
 }
