5f690ad093c1200e2665da4be33b3e49575b499d
Aditya Gupta <ag15035@gmail.com>
2023-03-18 00:35:09 +0530

    feat: separate common code

--- node.js
+++ node.js
@@ -21,21 +21,48 @@ function detailedSummary() {
     for (let e of data) if(!e.ignore && e.type != "comment") { total_expense[e.type] = parseFloat( (total_expense[e.type] + e.debit - e.credit).toFixed(2) ); will_be_back[e.type] += (e.will_be_back || 0); if(e.longterm) longterm[e.type] += e.debit-e.credit; }
 }
 
+/*
+ * @note It doesn't take into account any complex handling, such as
+ *       it does NOT take into account the fact that it is "ignore: true"
+ *       it does NOT take into account the fact that it is "type: comment"
+ *       it does NOT take into account the fact that it is "will_be_back"
+ *       it does NOT take into account the fact that it is "longterm"
+ *       it does NOT take into account the fact that it is "ignore: true"
+ *       it does NOT take into account the fact that it is "type: comment"
+ *
+ *       Do that yourself before passing the data array here
+ *
+ * */
+function getBriefSummary(dataArr, fieldName, reducerFn) {
+	let obj = {}
+
+	if ( !reducerFn ) {
+		// @note that both arguments have different types
+		reducerFn = (sum, expenseObj) => parseFloat( (sum + (expenseObj[fieldName] || 0)).toFixed(2) )
+	}
+
+	for (let e of dataArr) {
+		const idx = e["type"].indexOf("/")
+		const type = (idx == -1) ? e["type"] : e["type"].substr(0, idx)
+
+		obj[type] = reducerFn(obj[type] || 0, e)
+	}
+
+	return obj;
+}
+
 function briefSummary() {
-    for (let e of data) if(!e.ignore && e.type != "comment") {
-	    const idx = e.type.indexOf("/")
-	    const type = e.type.substr(0, (idx != -1) ? idx: e.type.length)
-	    total_expense[type] = 0;
-	    will_be_back[type] = 0
-	    longterm[type] = 0
-    }
-    for (let e of data) if(!e.ignore && e.type != "comment") {
-	    const idx = e.type.indexOf("/")
-	    const type = e.type.substr(0, (idx != -1) ? idx: e.type.length)
-	    total_expense[type] = parseFloat( (total_expense[type] + e.debit - e.credit).toFixed(2) );
-	    will_be_back[type] += (e.will_be_back || 0);
-	    if(e.longterm) longterm[type] = parseFloat( (longterm[type] + e.debit - e.credit).toFixed(2) );
-    }
+	total_expense = getBriefSummary(
+		data.filter(e => !e.ignore && e["type"] != "comment"), "",
+		(sum, expenseObj) => parseFloat( (sum + expenseObj["debit"] - expenseObj["credit"]).toFixed(2) )
+	)
+
+	will_be_back = getBriefSummary(data, "will_be_back")
+
+	longterm = getBriefSummary(
+		data.filter(e => e.longterm == true), "",
+		(sum, expenseObj) => parseFloat( (sum + expenseObj["debit"] - expenseObj["credit"]).toFixed(2) )
+	);
 }
 
 function getSalary() {
@@ -77,6 +104,7 @@ function main() {
     console.log("\n```");
     console.log("Salary: " + salary);
     console.log("Prev Month Leftover: " + prev_leftover);
+    
     console.log("");
     console.log("Total In: " + (salary + prev_leftover));
     console.log("Total Out: " + total_out);
